1bd614b60bff1af27bc2e1f5f643b3c6589877cf
Ken <kenjyco@gmail.com>
2020-10-12 17:25:35 -0500

    Bump to v0.1.11

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('README.rst', 'r') as fp:
 
 setup(
     name='bg-helper',
-    version='0.1.10',
+    version='0.1.11',
     description='Common CLI background helpers',
     long_description=long_description,
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/bg-helper',
-    download_url='https://github.com/kenjyco/bg-helper/tarball/v0.1.10',
+    download_url='https://github.com/kenjyco/bg-helper/tarball/v0.1.11',
     packages=find_packages(),
     install_requires=[
         'fs-helper',
