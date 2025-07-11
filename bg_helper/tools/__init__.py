f15ee23f589290cbf654c58281e42bbc12566236
Ken <kenjyco@gmail.com>
2025-07-11 00:46:22 -0500

    Remove reference to tools._sql in tools init

--- bg_helper/tools/__init__.py
+++ bg_helper/tools/__init__.py
@@ -4,5 +4,4 @@ from bg_helper.tools._grep import *
 from bg_helper.tools._pip import *
 from bg_helper.tools._ps import *
 from bg_helper.tools._pyenv import *
-from bg_helper.tools._sql import *
 from bg_helper.tools._ssh import *
