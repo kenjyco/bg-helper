cbfc4a2648a20302880ecbe2e907888ce9ece7b7
Ken <kenjyco@gmail.com>
2025-07-23 12:56:11 -0500

    Bump to v0.1.24

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ with open('requirements.txt', 'r') as fp:
 
 setup(
     name='bg-helper',
-    version='0.1.23',
+    version='0.1.24',
     description='CLI helpers for background tasks (shell), docker (databases), git, and SSH',
     long_description=long_description,
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/bg-helper',
-    download_url='https://github.com/kenjyco/bg-helper/tarball/v0.1.23',
+    download_url='https://github.com/kenjyco/bg-helper/tarball/v0.1.24',
     packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
